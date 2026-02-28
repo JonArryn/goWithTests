b7884c9bb9e4f8958bf1741adcbcb60e01e3d3a9
Jon Arryn <development@arryn.net>
2026-02-27 22:24:31 -0500

    Adds function to sum the total numbers in multiple slices

--- arrayAndSlice/sum.go
+++ arrayAndSlice/sum.go
@@ -1,11 +1,21 @@
 package arrayandslice
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
--- arrayAndSlice/sum_test.go
+++ arrayAndSlice/sum_test.go
@@ -1,14 +1,28 @@
 package arrayandslice
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+
+	numbers := []int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
 	want := 15
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
