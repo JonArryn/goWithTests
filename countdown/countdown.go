0e7cb4d87c10c8bcf0dcc174f2c24603f5e7d2cd
Jon Arryn <development@arryn.net>
2026-03-05 20:00:56 -0500

    implement countdown with sleep timer

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
+	time.Sleep(1 * time.Second)
 }
 
 func main() {
